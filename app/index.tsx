dcebde97e805c74d5c6138839b15e5025adaf077
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-06 10:34:27 +0900

    setup app

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View className={"w-full h-screen flex items-center justify-center"}>
+      <Text className={"text-4xl font-bold"}>Hello Expo!</Text>
+    </View>
+  );
+}
